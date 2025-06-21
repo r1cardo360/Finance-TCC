add743082fa59ac17e46d3b39c372cac42b77144
r1cardo360 <ric.furlanetto@hotmail.com>
2025-06-21 03:37:13 -0300

    Adicionando primeira rota parateste

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import userRoutes from './routes/authRoutes';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/apiFinanceira', userRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API rodando!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT} \n http://127.0.0.1:${PORT}`);
+});
